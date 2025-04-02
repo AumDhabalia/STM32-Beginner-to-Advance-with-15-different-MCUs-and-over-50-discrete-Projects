33d914664434bc85bd90c78690cd8091bbaf6b8a
Aum Dhabalia <aumdhabalia2307@gmail.com>
2025-04-02 10:56:20 +0530

    Update adc_setup.c

--- STM32F103C8T6/ADC/adc_setup.c
+++ STM32F103C8T6/ADC/adc_setup.c
@@ -1,10 +1,42 @@
+/*
+Initial steps before starting user code.
+*/
+
 #include "stm32f10x.h"
 
 int main(void)
 {
-  //Initialize Clock
-  
-  while(1)
-  {}
-  return 0;
+	//Initialize HSE 8 MHz
+	//Enable HSE
+	RCC->CR |= RCC_CR_HSEON;
+	while(!(RCC->CR & RCC_CR_HSERDY));
+	
+	//Configure Flash Latency
+	FLASH->ACR |= FLASH_ACR_LATENCY_2;
+	
+	//Configure PLL
+	RCC->CFGR |= RCC_CFGR_PLLSRC_HSE; // RCC->CFGR |= 0x0001 0000;
+	RCC->CFGR |= RCC_CFGR_PLLMULL9;   // RCC->CFGR |= 0x001C 0000;
+	
+	//Enable PLL
+	RCC->CR |= RCC_CR_PLLON;
+	while(!(RCC->CR & RCC_CR_PLLRDY));
+	
+	//Set PLL as System Clock
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while(!(RCC->CFGR & RCC_CFGR_SWS_PLL));
+	
+	//Configure AHB, APB1, APB2 Prescaler
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1;   // AHB Prescaler = 1 (72 MHz)
+  	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;  // APB1 Prescaler = 2 (36 MHz max)
+  	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;  // APB2 Prescaler = 1 (72 MHz)
+	
+	//Update System Core Clock variable
+	SystemCoreClock = 72000000;
+	
+	//User Code initialization...
+	while(1)
+	{
+		
+	}
 }
