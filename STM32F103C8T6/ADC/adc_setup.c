c47af2d0d6b8694d108ea430eebe1c86adadcfc7
Aum Dhabalia <aumdhabalia2307@gmail.com>
2025-05-06 12:09:06 +0530

    Update adc_setup.c

--- STM32F103C8T6/ADC/adc_setup.c
+++ STM32F103C8T6/ADC/adc_setup.c
@@ -34,16 +34,39 @@ int main(void)
 	//Update System Core Clock variable
 	SystemCoreClock = 72000000;
 
-	//Enable ADC1, PA0, AFIO
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN | RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
+	//Enable ADC1, GPIOA, AFIO
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 
 	//Set ADC1 clock to 12MHz
 	RCC->CFGR |= RCC_CFGR_ADCPRE_DIV6;
 
 	//Configure GPIOA as Input Analog Mode
-	GPIOA->CRL &= ~(0x0000000F);
+	GPIOA->CRL &= ~(0x0000000F); //Input Channel 0, PA0
 	GPIOA->CRL |= 0x00000008;
 
+	//Enable ADC1 Interrupt...
+	ADC1->CR1 |= ADC_CR1_EOCIE;
+	NVIC_EnableIRQ(ADC1_2IEQn);
+
+	//Set Sampling Rate for Channel 0...
+	ADC1->SMPR2 |= ADC_SMPR_SMP0_2 | ADC_SMPR2_SMP0_0 | ADC_SMPR2_SMP0_1;
+
+	//Set Sequence of the Channel...
+	ADC1->SQR3 |= 0x00000000;
+
+	//Enable ADC1 and set to continuous mode
+	ADC1->CR2 |= ADC_CR2_ADON | ADC_CR2_CONT;
+	for(volatile int i = 0;i < 1000;i++);
+
+	//Enable for second time (Actual enable)...
+	ADC1->CR2 |= ADC_CR2_ADON;
+	for(volatile int i = 0;i < 1000;i++);
+
+	//Calibrate
+	ADC1->CR2 |= ADC_CR2_CAL;
+	for(volatile int i = 0;i < 2000;i++);
 	
 	//User Code initialization...
 	while(1)
