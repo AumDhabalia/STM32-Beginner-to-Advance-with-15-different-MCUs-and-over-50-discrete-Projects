f43afb1a50f2fe586c7920493ab98e286a0a57ce
Aum Dhabalia <aumdhabalia2307@gmail.com>
2025-07-03 13:05:06 +0530

    Update segment7x4display.c

--- STM32F103C8T6/Blinky/segment7x4display.c
+++ STM32F103C8T6/Blinky/segment7x4display.c
@@ -3,7 +3,7 @@ Project Title : 7 Segment Display
 Author : Aum Dhabalia
 Date : 10/04/2025
 This program is for interfacing 4 x 7 Segment display. The display module used is common cathode type. Digital HIGH is sent for flashing diodes of the display.
-Here, PA0 to PA7 are connected to the display module from Pin A to G with dot at PA7 and CC to GND. PB4 to PB7 pins are used for digit selection.
+Here, PA0 to PA7 are connected to the display module from Pin A to G with dot at PA7 and CC to GND. PB12 to PB15 pins are used for digit selection.
 Refer RM0008 reference manual as well as STM32F103C8T6 datasheet.
 Refer stm32f10x.h file for macros used.
 -------------------------------------------------------
@@ -22,10 +22,10 @@ Refer stm32f10x.h file for macros used.
 |  8  | 1/0 |  1  |  1  |  1  |  1  |  1  |  1  |  1  |
 |  9  | 1/0 |  1  |  1  |  0  |  1  |  1  |  1  |  1  |
 -------------------------------------------------------
-PB4 -> Digit 0
-PB5 -> Digit 1
-PB6 -> Digit 2
-PB7 -> Digit 3
+PB15 -> Digit 0
+PB14 -> Digit 1
+PB13 -> Digit 2
+PB12 -> Digit 3
 */
 
 #include "stm32f10x.h"
@@ -56,8 +56,8 @@ int main(void)
 	
 	//Configure AHB, APB1, APB2 Prescaler
 	RCC->CFGR |= RCC_CFGR_HPRE_DIV1;   // AHB Prescaler = 1 (72 MHz)
-  	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;  // APB1 Prescaler = 2 (36 MHz max)
-  	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;  // APB2 Prescaler = 1 (72 MHz)
+  RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;  // APB1 Prescaler = 2 (36 MHz max)
+  RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;  // APB2 Prescaler = 1 (72 MHz)
 	
 	//Update System Core Clock variable
 	SystemCoreClock = 72000000;
@@ -68,10 +68,10 @@ int main(void)
 
 	//Configure PA0 to PA7 and PB4 to PB7 as Genral-Purpose Output Push-Pull mode (2 MHz)
 	GPIOA->CRL &= ~(0xFFFFFFFF);
-	GPIOB->CRL &= ~(0xFFFF0000);
+	GPIOB->CRH &= ~(0xFFFF0000);
 
 	GPIOA->CRL |= (0x22222222);
-	GPIOB->CRL |= (0x22220000);
+	GPIOB->CRH |= (0x22220000);
 	
 	//User Code initialization...
 	while(1)
@@ -80,33 +80,35 @@ int main(void)
 
 		while(count < 10000)
 		{
-			volatile int digit0 = count%10;         //Units digit at PB4
-			volatile int digit1 = (count%100)/10;   //Tens LS digit at PB5
-			volatile int digit2 = (count%1000)/100; //Hundreds digit at PB6 
-			volatile int digit3 = count/1000;       //Thousands digit at PB7
+			volatile int digit0 = count%10;         //Units digit at PB15
+			volatile int digit1 = (count%100)/10;   //Tens LS digit at PB14
+			volatile int digit2 = (count%1000)/100; //Hundreds digit at PB13 
+			volatile int digit3 = count/1000;       //Thousands digit at PB12
 			
 			//Time delay of 1000 ms...
-			for(volatile int j = 0;j < 250;j++)
+			//The delay here is adjusted based on the instruction cycles. Here, 910 iterations are executed instead of 1000.
+			//The reason being synchronous to the real time clock measurements.
+			for(volatile int j = 0;j < 910;j++)
 			{
-				GPIOB->ODR |= 0x000000E0;
+				GPIOB->ODR = 0x00007000;
 				GPIOA->ODR = num[digit0];
 				//Units Digit for 1 ms
 				for(volatile int i = 0;i < 1000;i++);
 
-				GPIOB->ODR = (GPIOB->ODR^0x000000E0) | 0x000000D0;
+				GPIOB->ODR = 0x0000B000;
 				GPIOA->ODR = num[digit1];
 				//Tens Digit for 1 ms
 				for(volatile int i = 0;i < 1000;i++);
 
-				GPIOB->ODR = (GPIOB->ODR^0x000000D0) | 0x000000B0;
+				GPIOB->ODR = 0x0000D000;
 				GPIOA->ODR = num[digit2];
 				//Hundreds Digit for 1 ms
 				for(volatile int i = 0;i < 1000;i++);
 
-				GPIOB->ODR = (GPIOB->ODR^0x000000B0) | 0x00000070;
+				GPIOB->ODR = 0x0000E000;
 				GPIOA->ODR = num[digit3];
 				//Thousands Digit for 1 ms
-				for(volatile int i = 0;i < 1000;i++)
+				for(volatile int i = 0;i < 1000;i++);
 			}
 			count++;
 		}
