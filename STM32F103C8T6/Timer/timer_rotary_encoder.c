596b65e6b5295d22872ea026379110ecc1fc98af
Aum Dhabalia <aumdhabalia2307@gmail.com>
2026-09-12 22:05:51 +0530

    Update comments for encoder interface project
    Added project title, author, date, and description for the encoder interface mode.

--- STM32F103C8T6/Timer/timer_rotary_encoder.c
+++ STM32F103C8T6/Timer/timer_rotary_encoder.c
@@ -1,5 +1,11 @@
 /*
-Initial steps before starting user code.
+Project Title : Encoder Interface Mode
+Author : Aum Dhabalia
+Date : 31/03/2025
+This program is for setting timers in encoder interface mode to detect direction of a rotary sensor.
+A rotory encoder is interfaced in pullup configuration with PA0 PA1.
+Refer RM0008 reference manual as well as STM32F103C8T6 datasheet.
+Refer to stm32f10x.h file for macros used.
 */
 
 #include "stm32f103xb.h"
